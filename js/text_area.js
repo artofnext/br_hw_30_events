2d5775162e18cd45520905bc28fef0cdba420dff
artofnext <novnovalex@gmail.com>
2019-11-14 15:12:38 +0200

    Init commit

---
+++ js/text_area.js
@@ -0,0 +1,36 @@
+let divText = document.getElementById('text_div');
+// let divText1 = document.getElementById('text_div1');
+let areaText = document.getElementById('text_area');
+
+// areaText.style.overflow = 'hidden';
+// areaText.style.resize = 'none';
+
+divText.addEventListener('click',activateInput);
+areaText.addEventListener('input',resizeArea);
+// areaText.style.display = 'none';
+
+function activateInput(e) {
+    // divText.style.visibility = 'hidden';
+    // areaText.style.display = 'initial';
+    areaText.focus();
+    
+}
+
+function resizeArea(e) {
+
+    divText.innerText = areaText.value;
+
+    // divText1.innerText = areaText.value;
+    // areaText.style.height = divText.offsetHeight + 'px';
+    // areaText.style.width = divText.offsetWidth + 'px';
+    // areaText.rows = divText.offsetHeight / 12;
+    // areaText.style.width = divText.scrollWidth + 'px';
+    // console.log('Div Height: ' + divText.offsetHeight + 'px');
+    // console.log('Div width: ' + divText.offsetWidth + 'px');
+    // console.table(e.scrollWidth);
+}
+
+// alert('Here!')
+
+
+
