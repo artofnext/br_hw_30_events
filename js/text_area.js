77aeec9c3a5d4427f353fd2544a901b838f586c5
artofnext <novnovalex@gmail.com>
2019-11-17 15:27:20 +0200

    textarea done

--- js/text_area.js
+++ js/text_area.js
@@ -6,15 +6,47 @@ areaText.style.overflow = 'hidden';
 areaText.style.resize = 'none';
 
 divText.addEventListener('click',activateInput);
+document.body.addEventListener('keydown',keypressHandler);
 areaText.addEventListener('input',resizeArea);
 areaText.style.display = 'none';
 
 function activateInput(e) {
+    // divText.style.visibility = 'hidden';
+    // divText.style.display = 'none';
+    // areaText.style.display = 'initial';
+    // areaText.value = divText.innerText.trim();
+    // areaText.focus();
+    activateTextarea();
+    
+}
+
+function keypressHandler(e) {
+    if (e.code == "KeyE" && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        console.log("Ctr + E pressed");
+        activateTextarea();
+    }
+    if (e.code == "KeyS" && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        console.log("Ctr + S pressed");
+        deactivateTextarea();
+    }
+}
+
+function activateTextarea() {
     divText.style.visibility = 'hidden';
     divText.style.display = 'none';
     areaText.style.display = 'initial';
+    areaText.value = divText.innerText.trim();
     areaText.focus();
-    
+}
+
+function deactivateTextarea() {
+    divText.style.visibility = 'visible';
+    divText.style.display = 'initial';
+    areaText.style.display = 'none';
+    divText.innerText.trim() = areaText.value;
+    // areaText.focus();
 }
 
 function resizeArea(e) {
